bd9d9ec72034ec7be5df60c6d7f262fc8fb4e9ac
Pablo Cha <pablo.cha@mercadolibre.com>
2021-09-27 12:03:51 -0300

    ci: follow integration test execution and fail

--- run-integration-tests.sh
+++ run-integration-tests.sh
@@ -2,6 +2,6 @@
 alias dc="docker-compose -f ./docker-compose-test.yml"
 sn=api
 dc up -d
-dc logs $sn
+dc logs -f $sn
 res=$(docker inspect --format "{{.State.ExitCode}}" "$(dc ps -q $sn)")
 exit $res
